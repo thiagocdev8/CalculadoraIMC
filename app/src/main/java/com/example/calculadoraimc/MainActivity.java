1410cc44096cffbb87dcfd4e95913233967ba545
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-08-31 17:27:23 -0300

    Another step forward

--- app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.calculadoraimc;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText editTextWeight = findViewById(R.id.edittext_weight);
+        SeekBar seekBarHeight = findViewById(R.id.seekbar_height);
+        Button buttonClear = findViewById(R.id.button_clear);
+        Button buttonCalculate = findViewById(R.id.button_calculate);
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextWeight.setText("");
+                seekBarHeight.setProgress(0);
+            }
+        });
+
+
+
     }
 }
